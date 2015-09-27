9f38955f9d51ce567217eccebf6e3bc84338bb9d
Chandra Galbiati <chandra.galbiati@gmail.com>
2015-09-26 21:00:36 -0400

    drawing and getting elev obj working

--- browser/js/common/factories/map.js
+++ browser/js/common/factories/map.js
@@ -1,95 +1,190 @@
-app.factory('MapFactory', function () {
+app.factory('MapFactory', function ($http) {
     var map;
-    var drawingManager;
+    var elevator;
+    // var drawingManager;
+    var poly; //in prog line
+    var points = []; // in prog latLng Arr
+
+
+
     function initMap(opt) {
-        // var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-      map = new google.maps.Map(document.getElementById('map-canvas'), opt);
-    // var newLoc = new google.maps.LatLng(25, 70);
-      // map.panTo(newLoc)
+        map = new google.maps.Map(document.getElementById('map-canvas'), opt);
+        elevator = new google.maps.ElevationService;
+        var infowindow = new google.maps.InfoWindow({map: map});
 
-       var elevator = new google.maps.ElevationService;
-      var infowindow = new google.maps.InfoWindow({map: map});
+    }
 
-      // Add a listener for the click event. Display the elevation for the LatLng of
-      // the click inside the infowindow.
-      map.addListener('click', function(event) {
-        displayLocationElevation(event.latLng, elevator, infowindow);
+    function seePointElevation (){
+        // Add a listener for the click event. Display the elevation for the LatLng of
+        // the click inside the infowindow.
+        map.addListener('click', function(event) {
+            displayLocationElevation(event.latLng, elevator, infowindow);
+        });
+    }
+
+
+    ///find out how to remove a certain click listener
+    // google.maps.event.clearListeners(map, 'bounds_changed');
+
+
+    function displayLocationElevation(location, elevator, infowindow) {
+      // Initiate the location request
+      elevator.getElevationForLocations({
+        'locations': [location]
+      }, function(results, status) {
+        infowindow.setPosition(location);
+        if (status === google.maps.ElevationStatus.OK) {
+          // Retrieve the first result
+          if (results[0]) {
+            // Open the infowindow indicating the elevation at the clicked position.
+            infowindow.setContent('The elevation at this point <br>is ' +
+                Math.floor(Number(results[0].elevation)*3.28084) + ' feet.');
+          } else {
+            infowindow.setContent('No results found');
+          }
+        } else {
+          infowindow.setContent('Elevation service failed due to: ' + status);
+        }
       });
-
     }
 
-
-function displayLocationElevation(location, elevator, infowindow) {
-  // Initiate the location request
-  elevator.getElevationForLocations({
-    'locations': [location]
-  }, function(results, status) {
-    infowindow.setPosition(location);
-    if (status === google.maps.ElevationStatus.OK) {
-      // Retrieve the first result
-      if (results[0]) {
-        // Open the infowindow indicating the elevation at the clicked position.
-        infowindow.setContent('The elevation at this point <br>is ' +
-            results[0].elevation + ' meters.');
-      } else {
-        infowindow.setContent('No results found');
-      }
-    } else {
-      infowindow.setContent('Elevation service failed due to: ' + status);
-    }
-  });
-}
-    
     function makePolyline (opt) {
+        //draws a polyline on the map given an options obj
+        console.log('making poly', opt.path);
         var path =new google.maps.Polyline(opt);
-        path.setMap(map)
+        path.setMap(map);
     }
 
     function move (loc) {
+        //centers map on a new location ({lat: , Lng: })
         var newLoc = new google.maps.LatLng(loc.lat, loc.lng);
-        console.log(newLoc)
+        console.log(newLoc);
         map.panTo(newLoc);
     }
 
-    function setUpDrawing(){
-        var drawingManager = new google.maps.drawing.DrawingManager({
-            drawingMode: google.maps.drawing.OverlayType.MARKER,
-            drawingControl: true,
-            drawingControlOptions: {
-              position: google.maps.ControlPosition.TOP_CENTER,
-              drawingModes: [
-                google.maps.drawing.OverlayType.MARKER,
-                google.maps.drawing.OverlayType.CIRCLE,
-                google.maps.drawing.OverlayType.POLYGON,
-                google.maps.drawing.OverlayType.POLYLINE,
-                google.maps.drawing.OverlayType.RECTANGLE
-              ]
-            },
-            markerOptions: {icon: 'images/beachflag.png'},
-            circleOptions: {
-              fillColor: '#ffff00',
-              fillOpacity: 1,
-              strokeWeight: 5,
-              clickable: false,
-              editable: true,
-              zIndex: 1
-            }
-          });
-        drawingManager.setMap(map);
-    }
-
-    function stopDrawing(){
-        drawingManager.setOptions({
-            drawingControl: false
-        });
-    }
-
     function startDrawing(){
-        drawingManager.setOptions({
-            drawingControl: true
+
+        //adds click listener to start constructing polyline and adding to points
+        poly = new google.maps.Polyline({
+            //red line
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 3
         });
+        poly.setMap(map);
+
+        // Add a listener for the click event to add to the line's path
+        map.addListener('click', addLatLng);
+        console.log('start drawing')
     }
+
+    function addLatLng(event) {
+        //adds a pt to the poly path and points array
+      var path = poly.getPath();
+
+      // Because path is an MVCArray, we can simply append a new coordinate
+      // and it will automatically appear.
+      path.push(event.latLng);
+      points.push(event.latLng);
+      console.log(' adding latlng', event.latLng)
+      // console.log(poly)
+    }
+
+    // 
+
+    function getElevPath(cb){
+    console.log('getting elev path for ', points)
+      elevator.getElevationAlongPath({
+        'path': points,
+        'samples': 256
+      }, function(ele){
+        return cb(ele);
+      });
+    }
+    
+      // var pathOpt = {
+      //   path: points,
+      //   geodesic: true,
+      //   strokeColor: '#FF0000',
+      //   strokeOpacity: 1.0,
+      //   strokeWeight: 1.5,
+      //   draggable: true,
+      // }
+      // makePolyline(pathOpt)
+
+
+   
+    return {
+        initMap: initMap,
+        makePolyline: makePolyline,
+        move: move,
+        startDrawing: startDrawing,
+        addLatLng: addLatLng,
+        // setUpDrawing: setUpDrawing,
+        getElevPath: getElevPath,
+        seePointElevation : seePointElevation,
+    };
+
+});
+
+    // function setUpDrawing(){
+    //     var drawingManager = new google.maps.drawing.DrawingManager({
+    //         drawingMode: google.maps.drawing.OverlayType.MARKER,
+    //         drawingControl: true,
+    //         drawingControlOptions: {
+    //           position: google.maps.ControlPosition.TOP_CENTER,
+    //           drawingModes: [
+    //             google.maps.drawing.OverlayType.MARKER,
+    //             google.maps.drawing.OverlayType.POLYLINE,
+    //           ]
+    //         },
+    //         markerOptions: {icon: 'images/beachflag.png'},
+    //         circleOptions: {
+    //           fillColor: '#ffff00',
+    //           fillOpacity: 1,
+    //           strokeWeight: 5,
+    //           clickable: false,
+    //           editable: true,
+    //           zIndex: 1
+    //         }
+    //       });
+    //     drawingManager.setMap(map);
+    //     var line = []
+
+    //     google.maps.event.addListener(drawingManager, "overlaycomplete", function(event) {
+    //             var newLine = event.overlay;
+    //             // console.log('latlng', event.overlay)
+    //             // event.overlay.latLngs.j[0].j.forEach(function(latlng){
+    //             //     // line.push(new google.maps)
+    //             // })
+    //             console.log('overlay', event.overlay)
+    //             newLine.type = event.type;
+
+    //             // getElevPath(event.overlay.getPath());
+    //             // console.log('x' , x);
+    //         });
+
+
+
+    //         // google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+    //         //     console.log('vert')
+    //         //     console.log(document.getElementById('vertices'))
+    //         //     // document.getElementById('vertices').val(event.overlay.getPath().getArray());
+    //         // });
+    // }
+
+     // function stopDrawing(){
+    //     drawingManager.setOptions({
+    //         drawingControl: false
+    //     });
+    // }
+
+    // function startDrawing(){
+    //     drawingManager.setOptions({
+    //         drawingControl: true
+    //     });
+    // }
   //   var flightPath = new google.maps.Polyline({
   //   path: flightPlanCoordinates,
   //   geodesic: true,
@@ -99,11 +194,5 @@ function displayLocationElevation(location, elevator, infowindow) {
   // });
 
   // flightPath.setMap(map);
-    return {
-        initMap: initMap,
-        makePolyline: makePolyline,
-        move: move,
-        setUpDrawing: setUpDrawing,
-    };
-});
+
 	
--- browser/js/main/main.js
+++ browser/js/main/main.js
@@ -1,199 +1,115 @@
 app.controller('MainCtrl', function ($scope, $state, MapFactory, AreaFactory, skiAreas){
 	$scope.skiAreas = skiAreas;
-	
+
 	function setCenter(newLoc){
 		return MapFactory.setCenter(newLoc);
 	}
 
-	$scope.move=MapFactory.move;
+
+	$scope.move = MapFactory.move;
 	var gpsStart = new google.maps.LatLng(39.88,-105.78);
 
-	// var gpsStart = {latitude: 39.88, longitude: -105.78};
-	// $scope.map = { center: { latitude: 25, longitude: -77 }, zoom: 8 };
 	var myOptions = {
 	  zoom : 13,
 	  center : gpsStart,
 	  panControl: true,
-	  mapTypeId: google.maps.MapTypeId.HYBRID
+	  mapTypeId: google.maps.MapTypeId.HYBRID,
+	  rotateControl: true,
+	  tiltControl: true,
 	};
 
 	MapFactory.initMap(myOptions);
 
 	$scope.map = myOptions;
 
-	var pathcoords = [
-    {lat: 39.88, lng: -105.78},
-    {lat: 39.78, lng: -105.88},
-    {lat: 39.68, lng: -105.98},
-  ];
+	// var pathcoords = [
+ //    {lat: 39.88, lng: -105.78},
+ //    {lat: 39.78, lng: -105.88},
+ //    {lat: 39.68, lng: -105.98},
+ //  ];
 
-	var pathOpt = {
-    path: pathcoords,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
-  }
-  MapFactory.makePolyline(pathOpt);
+	// var pathOpt = {
+ //    path: pathcoords,
+ //    geodesic: true,
+ //    strokeColor: '#FF0000',
+ //    strokeOpacity: 1.0,
+ //    strokeWeight: 1.5,
+ //    draggable: true,
+ //  }
+ //  MapFactory.makePolyline(pathOpt);
 
-  MapFactory.setUpDrawing();
+	$scope.startDrawing = MapFactory.startDrawing;
 
-// 	uiGmapGoogleMapApi.then(function(maps) {
-// 		// var userLatLng = new maps.LatLng(position.coords.latitude, position.coords.longitude);
-// 		// var userLatLngCenterObj = {latitude: userLatLng.A, longitude: userLatLng.F};
-// 		console.log('mapsObj', maps)
-// 		// console.log(maps.Map.prototype.setMapTypeId.toString())
-// 		// $scope.map.zoom = 7;
-// 		// $scope.map = {
-// 	 //  zoom : 10,
-// 	 //  center : gpsStart,
-// 	 //  panControl: true,
-// 	  mapTypeId : maps.MapTypeId.SATELLITE,
-// 	  // styles: styleArr
-// 	// };
-// 	var map_canvas_obj = document.getElementById("map");
-// 	var userLatLng = new maps.LatLng(25,75);
-// 	console.log('user', userLatLng.lat())
-// 	var userLatLngCenterObj = {latitude: userLatLng.A, longitude: userLatLng.F};
 
-// 	// $scope.marker = {
-// 	// 	id: 1,
-// 	// 	coords: gpsStart
-// 	// }
+  // MapFactory.setUpDrawing();
 
-// 	// var elevator = new maps.ElevationService;
-
-// 	// console.log(maps.Map.prototype.setCenter)
-// 	// var mapOpt = {
-// 	//   zoom : 10,
-// 	//   center : userLatLngCenterObj,
-// 	//   panControl: true,
-// 	//   options: {mapTypeId: google.maps.MapTypeId.SATELLITE },
-// 	//   // styles: styleArr
-// 	// };
-
-// 	$scope.map = new maps.Map(map_canvas_obj, {center: userLatLng, zoom: 8 });
-// 	// console.log('$scope.map', $scope.map)
-// 	// var marker = new maps.Marker({
-//  //          map : $scope.map,
-//  //          position : userLatLng,
-//  //    });
-
-// 		// $scope.map.setMapTypeId(maps.MapTypeId.SATELLITE);
-// 		// console.log($scope.map.getCenter())
-
-// 	     $scope.markers = [];
-
-// 	     // $scope.map.setMapTypeId(maps.MapTypeId.TERRAIN)
-
-// 	     console.log($scope.map)
-
-//       // var infoWindow = new maps.InfoWindow();
-
-//       // $scope.addMarker = function(lat, lng, title) {
-
-//       //   var latLang = new maps.LatLng(lat, lng);
-
-//       //   var marker = new maps.Marker({
-//       //     map : $scope.map,
-//       //     position : latLang,
-//       //     title : title
-//       //   });
-//       //   marker.content = '<div class="infoWindowContent">'
-//       //       + marker.title + '</div>';
-
-//       //   google.maps.event.addListener(marker, 'click', function() {
-//       //     infoWindow.setContent('<h2>' + marker.title + '</h2>'
-//       //         + marker.content);
-//       //     infoWindow.open($scope.map, marker);
-//       //   });
-
-//       //   $scope.markers.push(marker);
-
-//       //   $scope.map.setCenter(latLang);
-//       // };
-
-// // $scope.map = {
-// // 	  zoom : 7,
-// // 	  center : {latitude: 29.88, longitude: -105.78},
-// // 	  panControl: true,
-// // 	  mapTypeId : maps.MapTypeId.SATELLITE,
-// // 	  styles: styleArr
-// // 	};
-// 	});
+  $scope.polyE = function(){
+  	MapFactory.getElevPath(function(ele){
+  		$scope.elevObj = ele;
+  	});
+  };
+  	
 
 
 
-
-
-	// $scope.$on('mapInitialized', function(event, map) {
-	// 	var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	// 	console.log('after', userLatLng)
-	// 	map.center = userLatLng;
- //      	// var myLatlng = new google.maps.LatLng(40.705189,-74.009209)
-
- //      	// map.setCenter(myLatlng)
-      
- //    });
-
-	var styleArr = [{
-	  featureType: "landscape",
-	  stylers: [{
-	    saturation: -100
-	  }, {
-	    lightness: 60
-	  }]
-	}, {
-	  featureType: "road.local",
-	  stylers: [{
-	    saturation: -100
-	  }, {
-	    lightness: 40
-	  }, {
-	    visibility: "on"
-	  }]
-	}, {
-	  featureType: "transit",
-	  stylers: [{
-	    saturation: -100
-	  }, {
-	    visibility: "simplified"
-	  }]
-	}, {
-	  featureType: "administrative.province",
-	  stylers: [{
-	    visibility: "off"
-	  }]
-	}, {
-	  featureType: "water",
-	  stylers: [{
-	    visibility: "on"
-	  }, {
-	    lightness: 30
-	  }]
-	}, {
-	  featureType: "road.highway",
-	  elementType: "geometry.fill",
-	  stylers: [{
-	    color: "#ef8c25"
-	  }, {
-	    lightness: 40
-	  }]
-	}, {
-	  featureType: "road.highway",
-	  elementType: "geometry.stroke",
-	  stylers: [{
-	    visibility: "off"
-	  }]
-	}, {
-	  featureType: "poi.park",
-	  elementType: "geometry.fill",
-	  stylers: [{
-	    color: "#b6c54c"
-	  }, {
-	    lightness: 40
-	  }, {
-	    saturation: -40
-	  }]
-	}];
-})
+	// var styleArr = [{
+	//   featureType: "landscape",
+	//   stylers: [{
+	//     saturation: -100
+	//   }, {
+	//     lightness: 60
+	//   }]
+	// }, {
+	//   featureType: "road.local",
+	//   stylers: [{
+	//     saturation: -100
+	//   }, {
+	//     lightness: 40
+	//   }, {
+	//     visibility: "on"
+	//   }]
+	// }, {
+	//   featureType: "transit",
+	//   stylers: [{
+	//     saturation: -100
+	//   }, {
+	//     visibility: "simplified"
+	//   }]
+	// }, {
+	//   featureType: "administrative.province",
+	//   stylers: [{
+	//     visibility: "off"
+	//   }]
+	// }, {
+	//   featureType: "water",
+	//   stylers: [{
+	//     visibility: "on"
+	//   }, {
+	//     lightness: 30
+	//   }]
+	// }, {
+	//   featureType: "road.highway",
+	//   elementType: "geometry.fill",
+	//   stylers: [{
+	//     color: "#ef8c25"
+	//   }, {
+	//     lightness: 40
+	//   }]
+	// }, {
+	//   featureType: "road.highway",
+	//   elementType: "geometry.stroke",
+	//   stylers: [{
+	//     visibility: "off"
+	//   }]
+	// }, {
+	//   featureType: "poi.park",
+	//   elementType: "geometry.fill",
+	//   stylers: [{
+	//     color: "#b6c54c"
+	//   }, {
+	//     lightness: 40
+	//   }, {
+	//     saturation: -40
+	//   }]
+	// }];
+});
